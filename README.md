185800eaf5a72727cdabd9200f3344b6c3c202ee
rakh1213 <rail.khamitov@netcracker.com>
2020-05-15 21:51:55 +0400

    1) default redirect to swagger-ui.html 2) change Readme file for local startup service

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pixonic_game_api
+
+### For local start
+```
+1) docker-compose -f ./stack.yml up 
+2) mvn spring-boot:run
+```
---
+++ src/main/java/org/mshassium/pixonic/game/api/controller/DevController.java
@@ -0,0 +1,16 @@
+package org.mshassium.pixonic.game.api.controller;
+
+import io.swagger.models.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping()
+public class DevController {
+
+    @RequestMapping("/")
+    public String redirectToSwagger(Model model) {
+        return "redirect:/swagger-ui.html";
+    }
+
+}
